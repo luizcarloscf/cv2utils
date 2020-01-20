72e33cf3d53dfb6848ec1a3de53eb0db5220651a
Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>
2020-01-20 14:32:28 -0300

    change package name

---
+++ cv2utils/__init__.py
@@ -0,0 +1,3 @@
+from cv2utils.haar_cascade import HaarCascadeFace
+
+__all__ = ["HaarCascadeFace"]
--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cvutils',
+    name='cv2utils',
     version='0.0.1',
     description='Face detector wrapper',
     long_description='Face detector wrapper',
-    url='https://github.com/luizcarloscf/cvutils.git',
+    url='https://github.com/luizcarloscf/cv2utils.git',
     author='Luiz Carlos Cosmi Filho',
     author_email='luizcarloscosmifilho@gmail.com',
     license='MIT',
-    packages=find_packages('cvutils'),
-    package_dir={'': 'cvutils'},
+    packages=find_packages('cv2utils'),
+    package_dir={'': 'cv2utils'},
     zip_safe=False,
     install_requires=[
         'opencv-python==4.1.2.*',
