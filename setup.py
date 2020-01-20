295b79571bd39aeeba287a9f759d96b96db5cfa3
Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>
2020-01-20 15:53:55 -0300

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cv2utils',
-    version='0.0.2',
+    version='0.0.3',
     description='Face detector wrapper',
     long_description='Face detector wrapper',
     url='https://github.com/luizcarloscf/cv2utils.git',
